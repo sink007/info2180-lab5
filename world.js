78688a5d377e6fd5514e50df30fad78ceadc28d1
sink007 <lhines2000@gmail.com>
2024-11-29 14:47:01 -0500

    exercise 5 complete

--- world.js
+++ world.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
     const lookup= document.getElementById("lookup");
+    const lookcities= document.getElementById("lookcities");
     const result= document.getElementById("result");
     const field= document.getElementById("country");
     lookup.addEventListener("click",function(){
         const country= field.value;
-        let clean = DOMPurify.sanitize(country);
         let url = 'http://localhost/info2180-lab5/world.php';
-        if (clean) { 
-            url += `?country=${clean}`;  
-        }
+        url += `?country=${country}&lookup=countries`;  
         fetch(url)
         .then(response => response.text())
         .then(data => {
@@ -17,6 +15,19 @@ document.addEventListener('DOMContentLoaded', function(){
         .catch(error => {
             console.log(error);
         });
+    })
 
+    lookcities.addEventListener("click",function(){
+        const country= field.value;
+        let url = 'http://localhost/info2180-lab5/world.php';
+        url += `?country=${country}&lookup=cities`;  
+        fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            result.innerHTML= data;
+        })
+        .catch(error => {
+            console.log(error);
+        });
     })
 })
--- world.php
+++ world.php
@@ -8,31 +8,56 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $country=  isset($_GET['country']) ? $_GET['country'] : '';
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$lookup= $_GET['lookup'];
+if ($lookup == "countries") {
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-?>
+  echo "<table>
+      <thead>
+          <tr>
+              <th>Name</th>
+              <th>Continent</th>
+              <th>Independence</th>
+              <th>Head of State</th>
+          </tr>
+      </thead>
+      <tbody>";
 
-<table>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Continent</th>
-        <th>Independence</th>
-        <th>Head of State </th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($results as $row): ?>
-        <tr>
-            <td><?= $row['name']?> </td>
-            <td><?= $row['continent']?></td>
-            <td><?= $row['independence_year']?></td>
-            <td><?= $row['head_of_state']?></td>
-        </tr>
-      <?php endforeach; ?>
-    </tbody>
+  foreach ($results as $row) {
+      echo "<tr>
+              <td>{$row['name']}</td>
+              <td>{$row['continent']}</td>
+              <td>{$row['independence_year']}</td>
+              <td>{$row['head_of_state']}</td>
+          </tr>";
+  }
 
-</table>
+  echo "</tbody></table>";
+} 
+
+else {
+  if ($lookup == "cities") {
+      $stmt = $conn->query("SELECT * FROM countries as co JOIN cities as ci ON co.code = ci.country_code WHERE co.name LIKE '%$country%';");
+      $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      echo "<table>
+          <thead>
+              <tr>
+                  <th>Name</th>
+                  <th>District</th>
+                  <th>Population</th>
+              </tr>
+          </thead>
+          <tbody>";
+      foreach ($results as $row) {
+          echo "<tr>
+                  <td>{$row['name']}</td>
+                  <td>{$row['district']}</td>
+                  <td>{$row['population']}</td>
+              </tr>";
+      }
+      echo "</tbody></table>";
+  }
+}
 
 
